cc1835793716d2290a900fc281a791c846dd6394
kzaman3055 <kzaman3055@gmail.com>
2022-05-23 23:18:05 +0600

    sidebar and footer

--- resources/views/superadmin/body/footer.blade.php
+++ resources/views/superadmin/body/footer.blade.php
@@ -1,8 +1,11 @@
 <footer class="main-footer">
-	<div class="pull-right d-none d-sm-inline-block">
-		<ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
-			
+    <div class="pull-right d-none d-sm-inline-block">
+        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+		
+		  <li class="nav-item">
+			<a class="nav-link" href="#">Dev Profile</a>
+		  </li>
 		</ul>
-	</div>
-	&copy; 2022 <a href="http://www.MechanicKoi.com"> <b> Mechanic Koi </b></a>. All Rights Reserved.
-</footer>
+    </div>
+	  &copy; 2022 <a href="#">HCMSC</a>. All Rights Reserved.
+  </footer>
